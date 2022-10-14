915f9c8499df139d4c925c6dcae21d34f4d0c132
BatuHanHub <114951572+BatuHanHub@users.noreply.github.com>
2022-10-15 01:42:35 +0300

    Add files via upload

---
+++ Asıl proje/3 zuppeye ozel.py
@@ -0,0 +1,24 @@
+#8.sınıf ingilizce projem (2021-2022)
+
+'''normalde ortada hata yok ben bu programı o 3 züppe için yazmıştım pişmanda değilim onlara versiyon 2 çok güzel size özel diye kakalıyacaktım 
+sırf o 3 züppe oynamasın diye ki haklıyım bu programı yazdım. benden özür bile dileseler kabul etmem hatta benim kanıma ihtiyaçları olsa VERMEM!''' 
+
+print("Takım adının ilk harfini BÜYÜK diğerlerini küçük harflere yazınız\n") #uyarı veriyor
+x = input("1.takımın adını giriniz \n$") #takım adı istiyor
+print("\nHata:Takım adına ulaşılamıyor\n") #"Hata:Takım adına ulaşılmıyor" ekrana yazdırıyor
+Hata = input('Düzeltmek için "Sudoİnfo" yazınız: \n$') 
+
+if Hata == "Sudoİnfo": #eğer "Sudoİnfo" yazarsanız aşağıdaki kodlar çalışır
+    print("Çözüm 1: https://youtu.be/dQw4w9WgXcQ \n")
+    print('Çözümler işe yaramazsa "Sudo" yazarak 2. Çözüm`e ulaşabilirsiniz. \n')
+    çözüm = input("$")
+    print("\nSorun ve Çözüm: sorun takımdaki insanlar bozuk ;) çözüm ise DEFOLUN GİDİN!")
+    
+#sonucunun böyle olacağını ben Efe ve Alper biliyorduk :)
+ 
+''''
+züppeler eğer bu kodu okuyorsanız ki sizde o akıl varsa, arkadaşlarınızla ve burda yazmasam iyi olacaklarla siz onları ve onlarla 
+ne yaptıklarınızı çok iyi biliyorsunuz onlarla beraber DEFOLUN! BİR DAHA KARŞIMA ÇIKMAYIN! 
+KENDİNİZE BAŞKA APTALLARA BULUN! GÖZÜMEDE GÖRÜNMEYİN!
+'''
+input("\n...")
